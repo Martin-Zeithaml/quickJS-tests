77c30447debf8fed60c45c84e71982d48fcdce04
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2023-11-08 17:14:28 +0100

    String tests
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- tests.js
+++ tests.js
@@ -1,47 +1,56 @@
 import * as tfs from './bin/libs/test_fs';
 import * as tshell from './bin/libs/test_shell';
-import * as tzos from './bin/libs/test_zos';
+import * as tstring from './bin/libs/test_string';
+import * as tzoslib from './bin/libs/test_zos';
 import * as tzosdataset from './bin/libs/test_zos-dataset';
 import * as tzosfs from './bin/libs/test_zos-fs';
 import * as tutils from './bin/utils/test_date-add';
 import * as log from './log'
 
-const TEST_FS = [
-    tfs.test_resolvePath, 
-    tfs.test_convertToAbsolutePath
-];
-
-const TEST_SHELL = [ tshell.test_execAnySync ];
-
-const TEST_ZOS = [ tzos.test_tsoCommand ];
-
-const TEST_ZOSDATASET = [
-    tzosdataset.test_validDataSetName,
-    tzosdataset.test_validDataSetMemberName,
-    tzosdataset.test_isDatasetExists,
-    tzosdataset.test_tsoIsDatasetExists
-];
-
-const TEST_ZOSFS = [ tzosfs.test_getFileEncoding ];
-
-const TEST_UTILS = [ tutils.test_dateAdd ];
-
-let TEST_ALL = [];
-TEST_ALL = TEST_ALL.concat(TEST_FS, TEST_SHELL, TEST_ZOS, TEST_ZOSFS, TEST_ZOSDATASET, TEST_UTILS);
+const TEST = {
+    fs: [ 
+        tfs.test_resolvePath, 
+        tfs.test_convertToAbsolutePath
+    ],
+    shell: [ 
+        tshell.test_execAnySync
+    ],
+    string: [ 
+        tstring.test_escapeDollar,
+        tstring.test_escapeRegExp
+    ],
+    zoslib: [ 
+        tzoslib.test_tsoCommand
+    ],
+    zosdataset: [
+        tzosdataset.test_validDataSetName,
+        tzosdataset.test_validDataSetMemberName,
+        tzosdataset.test_isDatasetExists,
+        tzosdataset.test_tsoIsDatasetExists
+    ],
+    zosfs: [
+        tzosfs.test_getFileEncoding
+    ],
+    utils: [
+        tutils.test_dateAdd
+    ]
+}
 
 let infos = [];
 let errors = [];
 let result;
 
-TEST_ALL.forEach(testFunction => {
-    if (typeof testFunction === 'function') {
-        result = testFunction();
-        infos = infos.concat(result.infos);
-        errors = errors.concat(result.errors);
-    } else {
-        console.log(`${testFunction.name} is not a function. Skipping the test...`);
+for (let lib in TEST) {
+    for (let testFunction in TEST[lib]){
+        if (typeof TEST[lib][testFunction] === 'function') {
+            result = TEST[lib][testFunction]();
+            infos = infos.concat(result.infos);
+            errors = errors.concat(result.errors);
+        } else {
+            console.log(`${testFunction.name} is not a function. Skipping the test...`);
+        }
     }
-})
+}
 
 console.log(log.CYAN + `*** INFOS(${infos.length}) ***`);
 for (let info in infos) {
