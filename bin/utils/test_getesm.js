40f1f89d80eb36d4bb0b6c8942c2e857a9e09f38
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2023-12-13 09:29:26 +0100

    Tester
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- bin/utils/test_getesm.js
+++ bin/utils/test_getesm.js
@@ -1,29 +1,19 @@
 import * as shell from '../../../bin/libs/shell';
-import * as log from '../../log';
+import * as tester from '../../tester';
+
+function getESMRexx(){
+    const result = shell.execOutSync('sh', '-c', "./bin/utils/getesm.rexx");
+    return result.out;
+}
 
 export function test_getesm() {
     const TESTS = {
-        setting : { binary: true},
+        setting : { binary: true },
         testset: {
-            t1: { parms: '-h', expected: { rc: 0, out: false } },
-            t2: { parms: '', expected: { rc: 0, out: true } },
-            t3: { parms: 'asdf', expected: { rc: 1, out: false } }
+            t1: { parms: '-h', expected: { rc: 0 } },
+            t2: { parms: '', expected: { rc: 0, out: getESMRexx() } },
+            t3: { parms: 'asdf', expected: { rc: 1 } }
         }
     }
-    let infos = [];
-    let errors = [];
-
-    for (let test in TESTS) {
-        const parms = TESTS[test].parms;
-        const result = shell.execOutSync('sh', '-c', `../bin/utils/getesm ${parms}`);
-        if (TESTS[test].expected.out) {
-            const expected = shell.execOutSync('sh', '-c', "./bin/utils/getesm.rexx");
-            log.infoAndErr(infos, errors, 'bin/utils/getesm', 'getesm', parms, result.out, expected.out)
-        } else {
-            log.infoAndErr(infos, errors, 'bin/utils/getesm', 'getesm', parms, result.rc, TESTS[test].expected.rc)
-        }
-        
-    }
-    
-    return { infos, errors }
+    return tester.process(TESTS, FILE, '../bin/utils/getesm');
 }
--- tester.js
+++ tester.js
@@ -31,48 +31,66 @@ function pushResults(infos, errors, file, functionName, parms, result, expected)
     }
 }
 
+function getFunctionName(name) {
+    if (typeof name === 'function')
+        return name.name;
+    if (typeof name === 'string')
+        return name;
+    return 'Unknown';
+}
+
 export function process(tests, file, functionName) {
     let infos = [];
     let errors = [];
-
-    if (typeof functionName === 'function') {
+    const functionType = typeof functionName;
+    if (functionType === 'function' || functionType === 'string') {
         for (let t in tests['testset']) {
             if (tests['testset'][t].parms?.parmsArray) {
                 let expected = tests['testset'][t].parms.expected;
                 for (let i = 0; i < tests['testset'][t].parms.parmsArray.length; i++ ) {
                     const parms = tests['testset'][t].parms.parmsArray[i];
                     const result = functionName(parms);
-                    pushResults(infos, errors, file, functionName.name, parms, result, expected);
+                    pushResults(infos, errors, file, getFunctionName(functionName), parms, result, expected);
                 }
             } else {
                 const parms = tests['testset'][t].parms;
                 let expected = tests['testset'][t].expected;
                 let result;
-                if (Array.isArray(parms)) {
-                    result = functionName(...parms);
-                }
-                else {
-                    if (tests['setting']?.shell)
-                        result = functionName('sh', '-c', parms);
-                    else
-                        result = functionName(parms);
+                if (functionType === 'function') {
+                    if (Array.isArray(parms)) {
+                        result = functionName(...parms);
+                    }
+                    else {
+                        if (tests['setting']?.shell)
+                            result = functionName('sh', '-c', parms);
+                        else
+                            result = functionName(parms);
+                    }
+                } else {
+                    result = shell.execOutSync('sh', '-c', `${functionName} ${parms}`)
                 }
                 if (tests['setting']?.shell) {
                     result = 'executed'
                     expected = 'executed'
                 }
-                if (tests['testset'][t].expected?.rc !== undefined) {
-                    expected = tests['testset'][t].expected.rc
-                    result = result.rc
+                if (tests['testset'][t].expected?.rc !== undefined || tests['testset'][t].expected?.out !== undefined) {
+                    if (tests['testset'][t].expected?.rc !== undefined) {
+                        expected = tests['testset'][t].expected.rc
+                        result = result.rc
+                        pushResults(infos, errors, file, getFunctionName(functionName), parms, result, expected)
+                    }
+                    if (tests['testset'][t].expected?.out !== undefined) {
+                        expected = tests['testset'][t].expected.out
+                        result = result.out
+                        pushResults(infos, errors, file, getFunctionName(functionName), parms, result, expected)
+                    }
+                } else {
+                    pushResults(infos, errors, file, getFunctionName(functionName), parms, result, expected)
                 }
-                if (tests['testset'][t].expected?.out !== undefined) {
-                    expected = tests['testset'][t].expected.out
-                    result = result.out
-                }
-                pushResults(infos, errors, file, functionName.name, parms, result, expected)
             }
         }
-    } else {
+    } 
+    if (typeof functionName === 'undefined') {
         infos.push(msgUndefined(file));
         errors.push(msgUndefined(file));
     }
